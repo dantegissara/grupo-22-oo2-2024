3ce969998b823d53c5e46ff65c560b2b2aeea5d7
Dante Gissara <dantegissara@outlook.es>
2024-05-07 22:22:11 -0300

    Feat: Commit Inicial

---
+++ src/main/java/com/unla/grupo22/tpc/Grupo22Oo22024Application.java
@@ -0,0 +1,13 @@
+package com.unla.grupo22.tpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo22Oo22024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo22Oo22024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/unla/grupo22/tpc/Grupo22Oo22024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo22.tpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo22Oo22024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
