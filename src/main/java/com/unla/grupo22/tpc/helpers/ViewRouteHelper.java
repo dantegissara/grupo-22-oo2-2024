75fdb4995eb0c35f9a2a37dcf8e789fa1d671fac
RezzanoMartin <m.rezzano100@gmail.com>
2024-06-03 21:00:28 -0300

    Login ( copiado de gustavo )

---
+++ src/main/java/com/unla/grupo22/tpc/helpers/ViewRouteHelper.java
@@ -0,0 +1,16 @@
+package com.unla.grupo22.tpc.helpers;
+
+public class ViewRouteHelper {
+	/**** Views ****/ // VISTAS. LAS VISTAS SE DEFINEN EN LA PARTE DE TEMPLATES
+	//HOME
+	public final static String INDEX = "home/index";
+	public final static String HELLO = "home/hello";
+
+	//USER
+	public final static String USER_LOGIN = "user/login";
+	public final static String USER_LOGOUT = "user/logout";
+
+	/**** Redirects ****/
+	public final static String ROUTE = "/index";
+
+}
