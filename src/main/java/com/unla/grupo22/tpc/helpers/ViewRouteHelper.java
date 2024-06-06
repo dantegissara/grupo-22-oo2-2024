4b625177552e05b7cdd7a4c5db6456061b3b47be
RezzanoMartin <m.rezzano100@gmail.com>
2024-06-06 19:38:50 -0300

    Commit con varios cambios

--- src/main/java/com/unla/grupo22/tpc/helpers/ViewRouteHelper.java
+++ src/main/java/com/unla/grupo22/tpc/helpers/ViewRouteHelper.java
@@ -9,6 +9,12 @@ public class ViewRouteHelper {
 	//USER
 	public final static String USER_LOGIN = "user/login";
 	public final static String USER_LOGOUT = "user/logout";
+	
+	//PRODUCTO
+	public final static String PRODUCTO_INDEX = "producto/index";
+	//public final static String PRODUCTO_NEW = "producto/new";
+	//public final static String PRODUCTO_UPDATE = "producto/update"; // seccion html en templates -> person -> update
+	//public final static String PRODUCTO_PARTIAL_VIEW = "producto/partialPersonView";
 
 	/**** Redirects ****/
 	public final static String ROUTE = "/index";
--- src/main/java/com/unla/grupo22/tpc/repositories/IProductoRepository.java
+++ src/main/java/com/unla/grupo22/tpc/repositories/IProductoRepository.java
@@ -1,11 +1,13 @@
 package com.unla.grupo22.tpc.repositories;
 
+import java.io.Serializable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.unla.grupo22.tpc.entities.Producto;
 
-@Repository
-public interface IProductoRepository extends JpaRepository<Producto, Integer> { // En lugar de Integer colocar Serializable ?
+@Repository("productoRepository")
+public interface IProductoRepository extends JpaRepository<Producto, Serializable> { // En lugar de Integer colocar Serializable ?
 	
 // find by name ?
 // find by ... ?
