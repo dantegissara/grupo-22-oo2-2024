370e2842b545a7c7e28c46b1ecff18e7690ca387
LeandroMussi99 <168138699+LeandroMussi99@users.noreply.github.com>
2024-06-17 20:09:30 -0300

    Actualizacion de html comprar agrega los precios y checkbox

--- src/main/java/com/unla/grupo22/tpc/controllers/CompraController.java
+++ src/main/java/com/unla/grupo22/tpc/controllers/CompraController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.unla.grupo22.tpc.entities.Compra;
 import com.unla.grupo22.tpc.entities.DetalleCompra;
@@ -46,24 +47,27 @@ public class CompraController {
 	        @RequestParam("userId") int userId,
 	        @RequestParam("productosIds") List<Integer> productosIds,
 	        @RequestParam("cantidades") List<Integer> cantidades,
-	        Model model) {
+	        RedirectAttributes redirectAttributes) {
 
 	    User user = userService.findById(userId).orElseThrow(() -> new RuntimeException("User no encontrado"));
 
 	    List<DetalleCompra> detallesCompra = productosIds.stream().map(productoId -> {
 	        Producto producto = productoService.findById(productoId).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
 	        int cantidad = cantidades.get(productosIds.indexOf(productoId));
-	        return new DetalleCompra(null, producto, cantidad);  // Compra se establecerá más adelante
+	        return new DetalleCompra(null, producto, cantidad);
 	    }).collect(Collectors.toList());
 
 	    Compra compra = new Compra(user, detallesCompra);
-	    detallesCompra.forEach(detalle -> detalle.setCompra(compra));  // Establecer la relación bidireccional
+	    detallesCompra.forEach(detalle -> detalle.setCompra(compra));
 
 	    compraService.realizarCompra(compra);
 
-	    model.addAttribute("success", "Compra realizada con éxito");
-	    return "compras/nueva";
+	    redirectAttributes.addFlashAttribute("success", "Compra realizada con éxito");
+	    return "redirect:/compras/nueva";
 	}
+
+
+
 
 
 
