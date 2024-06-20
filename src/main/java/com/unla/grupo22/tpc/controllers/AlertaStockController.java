7f6c12943d39e3b5594ff652d8e014be09bac100
nacho <nachogomezz0011@gmail.com>
2024-06-19 22:51:34 -0300

    yml actualizado

--- src/main/java/com/unla/grupo22/tpc/controllers/AlertaStockController.java
+++ src/main/java/com/unla/grupo22/tpc/controllers/AlertaStockController.java
@@ -3,13 +3,11 @@ package com.unla.grupo22.tpc.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import com.unla.grupo22.tpc.entities.Producto;
-import com.unla.grupo22.tpc.service.IProductoService;
 import com.unla.grupo22.tpc.services.implementation.ProductoService;
 
 @Controller
